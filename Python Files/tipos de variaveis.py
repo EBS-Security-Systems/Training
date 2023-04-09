df45d1de1245d9637ce386134d6899141bb94eba
Thiago Sousa <thiagosousa81@gmail.com>
2023-04-09 17:09:51 -0300

    Create tipos de variaveis.py

---
+++ Python Files/tipos de variaveis.py
@@ -0,0 +1,21 @@
+nome = "Thiago" # Isto é uma string (texto)
+idade = 17 # Isto é um int (inteiro)
+altura = 1.75 # Isto é um float (nº de ponto flutuante). Pode ser interpretado como double (nº real) tbm
+
+#podemos fazer de duas formas
+
+# 1º forma
+print('Meu nome é ', nome,'.')
+print('Minha idade é ', idade,'.')
+print('Minha altura é ', altura,'.')
+
+# Enfeitando o código ;)
+print('\n----------------------------\n')
+
+# 2º forma
+print(f"Meu nome é {nome}.")
+print(f"Minha idade é {idade}.")
+print(f"Minha altura é {altura}.")
+
+#Enfim, podem desenvolver este código a vontade
+
