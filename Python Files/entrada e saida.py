9807c0e5c4847ef9270174c15c58be295c140568
Thiago Sousa <thiagosousa81@gmail.com>
2023-04-10 15:06:17 -0300

    Create entrada e saida.py

---
+++ Python Files/entrada e saida.py
@@ -0,0 +1,17 @@
+# Conceitos de entrada/saída e processamento
+
+# Saída de informação:
+# É o mesmo que mostrar dados para o usuário, instruíndo-o sobre o que fazer no uso do programa
+# Exemplo:
+print('Digite um número:')
+
+# Entrada de informação:
+# É quando o usuário insere dados no programa para seu funcionamento
+num = input() # nesse caso o tipo de dado é int (inteiro)
+
+# Processamento:
+# É quando o programa processa as informações inseridas pelo usuário para realizar alguma função
+num = num + 9
+# Quando um dado é processado o resultado pode ser mostrado na tela
+print(f"o número que você digitou + 9 é {num}")
+
